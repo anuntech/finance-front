cfd643ff49c2a8a837b4581188ea03ee2b238c36
Alessandro Henrique Ramos <alessandro@anuntech.com>
2025-03-20 15:04:11 -0300

    feat: add more dates and update exports function (#44)

--- utils/format-balance.ts
+++ utils/format-balance.ts
@@ -1,5 +1,4 @@
 export const formatBalance = (balance: number) => {
-	const balanceParsed = Number.parseFloat(String(balance));
 	const balanceFormatted = new Intl.NumberFormat("pt-BR", {
 		style: "currency",
 		currency: "BRL",
